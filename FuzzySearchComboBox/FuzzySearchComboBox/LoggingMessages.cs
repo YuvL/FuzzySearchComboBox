151a83df48b3227f94f17beb2a1e1f6aa13a7674
tgduk <duk@protei.ru>
2017-08-28 15:40:35 +0300

    fix behavior of an element in a group: there was an incorrect selection list after cleaning the selected value in the another associated element

--- FuzzySearchComboBox/FuzzySearchComboBox/LoggingMessages.cs
+++ FuzzySearchComboBox/FuzzySearchComboBox/LoggingMessages.cs
@@ -6,6 +6,7 @@ namespace Controls.FuzzySearchComboBox
         internal static string ComboboxAddedToRelatedList = "Added to list of coupled: '{0}'";
         internal static string FuzzySearchComboBoxCreatedFormat = "Created: '{0}'";
         internal static string ParentItemSourceChangedInCombobox = "ParentItemSource changed: '{0}'";
+        internal static string ChildItemSourceChangedInCombobox = "ChildItemSource changed: '{0}'";
         internal static string SelectedItemSettedTo = "SelectedItem setted on '{1}' to '{0}'";
         internal static string ThereAreMoreThanComboBoxesInCoupledComboBoxes = "More than three coupled Comboboxes on '{0}' ({1})";
         internal static string ThereAreMoreThanTwoFiltersIn = "More than two filters on '{0}'";
